4a7e7f1d1316228531b4fd054465b6de82747341
Felipe Vieira <70296048+Felipecioccari@users.noreply.github.com>
2023-08-17 00:15:14 +0000

    adiciona scripts `elsint:check` e `eslint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para estudo acompanhando o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home (){
-    return <h1>Te amo muito Rafa, obrigado por existir e sempre me fazer feliz! </h1>
+function Home() {
+  return (
+    <h1>Te amo muito Rafa, obrigado por existir e sempre me fazer feliz! </h1>
+  );
 }
 
 export default Home;
