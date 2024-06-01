358d4d0631fba29187fcc0149f14a86e15c25755
Felipe Vieira Cioccari <felipecioccari@fineasytech.com>
2024-05-31 22:43:13 -0300

    added janjo photo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <div>
       <h1>Em construção</h1>
       <img
-        src="https://lh3.googleusercontent.com/pw/AP1GczNtcNAdX0Azbr0Lq34DZnaN4bMq1EzhkOjTZjtkoFOsR16Kl8YSGs4tU3j6LbOVp5vqAmz1xAQePhc7n-GwyluyUc6DRAC5NezwT4LWMyazMX1S8qne59Xzd9HPj9lpw2LzFX5Uo1QLunIe546ETzwzGH3zZPo5Qu0sgrSGAPRdr0zZA_i275USQNNw2VJ2jYq0KMWKs35h0lIDXKLIv-dXpDWVt8abv5kVUgUxovTPQDft3NfBbUdiVIEf6hHiUX5rKZRw5NLsyDcgM8f7T1fIy8tahn5kbem5GBRhbSAyOUnNUnCwBrmU2hI4LBYwUAe46UwxTZjmo5QL2lMgm07itPeo9g2ACVBCnFj7OBHdooaP3CTQfywMW9IIBHqIRv9JNgqxmIV4qpX-NEEq-s9FgZao1Kw1rMlO8fxGgoLLBbJ8nGjrzF0r0oRgfbSm48t6yt77GLYbQyotLKpV3z56st8ul4Tx69KW10S0_CsN6OqyrBPZ2YdrxfbYgQJa2zYRBX6_Z_eNbQKsg4mzwktCwIPB1Zu3rWSA-Nl-9j8MWDoHmviV526_ENTY1otjNB7exu01J-cpjZT5rTHBf_u3vJFRmZSGmTC3ZNo_7vIga2jaCTurX-qZwr3_ray9-DjzlVBrsXCwE4L0bRHZCYuLwDjFD1_Z3WDwdA-btOvDCmXoHncaVb5LE2iUjgu33QkoHu56Zdk3zdZmr_jh58jfLwyln8OcC7SpQwHuG0cZliOx-F6svV_UOh3bODaPpGQvOZHVZeSqicutFyTDM__VpefOwjr6kdVoUr-XluEJwA_4r5NpBaqcSddt5yNC3sYwRgMmhcDG2ZhD3gzzg5YIg60eZ1qhrgGy4WKzgbkxAuinTnoOAFZYCydOJ0RNdxpH78magj8wcV6V4JYo7UqHKQ=w449-h599-s-no-gm?authuser=0"
+        src="https://i.imgur.com/5g88ueC.jpeg"
         alt="janjo"
       />
     </div>
